98886baafcb9f37e03eaa0b53ec704024385679f
aegooby <aegooby@gmail.com>
2020-08-03 17:46:19 -0400

    pipfile

---
+++ dohamaps/README.md
@@ -0,0 +1,7 @@
+# dohamaps
+Multi-scale generative adversarial model using `tf.keras` to predict future urban growth from satellite map images.
+
+## Dependencies
+ * TensorFlow 2.2.0
+ * Pillow 7.2.0
+
---
+++ setup.py
@@ -0,0 +1,25 @@
+import  setuptools
+import  os
+
+with open(os.path.join("dohamaps", "README.md"), "r") as readme:
+    long_description = readme.read();
+
+setuptools.setup \
+(
+    name = "dohamaps",
+    version = "1.1.0",
+    author = "dohamaps",
+    author_email = "dohamaps20@gmail.com",
+    description = "Generative adversarial urban growth prediction of Doha.",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/dohamaps/dohamaps",
+    packages = setuptools.find_packages(),
+    classifiers = \
+    [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
